1464233d299986019f00101c7787f4b4665657a7
LeonardoFerrisi <biggestresurgence1492@gmail.com>
2022-10-09 23:45:09 -0400

    updated struct

--- main/main.py
+++ main/main.py
@@ -5,5 +5,17 @@
 
 
 # Interpet CSV with artist
+from tools.create import create_image_from_eeg
 
-from tools import create, create_montage
+from tools.create_montage import create_montage_from_eeg
+
+class artgen:
+
+    def __init__(self, mode=0):
+        """
+        Mode: 0 = single, 1 = montage
+        """
+
+        create_image_from_eeg()
+
+
